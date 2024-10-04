8fd3b57a3e6c755b67d2c6cd0c074244681d4eef
ArielSanchez0202 <ariel.sanchez@cloud.uautonoma.cl>
2024-10-04 05:33:09 -0300

    Lista de terpaeutas activos (Recepcionista)

--- recepcionista/views.py
+++ recepcionista/views.py
@@ -14,20 +14,31 @@ from datetime import date
 
 @role_required('Recepcionista')
 def recepcionista_terapeutas_activos(request):
-    '''query = request.GET.get('q', '')
+    query = request.GET.get('q', '')
+    orden = request.GET.get('orden', 'user__first_name')  # Orden por defecto: nombre
+    # Validar el campo de orden
+    valid_order_fields = ['user__first_name', 'user__last_name', 'user__profile__rut', 'especialidad', 'fecha_contratacion']
+    if orden not in valid_order_fields:
+        orden = 'user__first_name'
     if query:
-        terapeuta = Terapeuta.objects.filter(
-            estado='activo',
-            nombre__icontains=query
-        ) | Terapeuta.objects.filter(
-            estado='activo',
-            rut__icontains=query
-        )
+        # Filtrar por nombre, apellido o rut
+        terapeutas = Terapeuta.objects.filter(
+            Q(user__first_name__icontains=query) | 
+            Q(user__last_name__icontains=query) | 
+            Q(user__profile__rut__icontains=query)
+        ).order_by(orden)
     else:
-    '''
-    terapeuta = Terapeuta.objects.all()
+        # Mostrar todos los terapeutas si no hay búsqueda, ordenando por el valor seleccionado
+        terapeutas = Terapeuta.objects.all().order_by(orden)
 
-    return render(request, 'recepcionista_terapeutas_activos.html', {'terapeuta': terapeuta})
+    # Paginación
+    paginator = Paginator(terapeutas, 5)  # 5 terapeutas por página
+    page_number = request.GET.get('page')  # Obtener el número de página de la URL
+    page_obj = paginator.get_page(page_number)  # Obtener la página correspondiente
+    context = {
+        'terapeutas': page_obj
+    }
+    return render(request, 'recepcionista_terapeutas_activos.html', context)
 
 
 def calcular_edad(fecha_nacimiento):
