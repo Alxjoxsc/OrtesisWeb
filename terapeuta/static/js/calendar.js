87c504dd49ec53cf36f145d2a40a9af7cd1ea73e
Alxjoxsc <alejandro.escobedo@cloud.uautonoma.cl>
2024-10-14 23:26:46 -0300

    reinicio de agenda, queda en blanco sin funciones

--- terapeuta/static/js/calendar.js
+++ terapeuta/static/js/calendar.js
@@ -6,16 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const calendar = document.getElementById('calendar');
     const weekViewButton = document.getElementById('week-view');
     const monthViewButton = document.getElementById('month-view');
-    const popup = document.getElementById('popup');
-    //const btnAgendar = document.getElementById('add');
-    const popupEditarCita = document.getElementById('popupEditarCita');  // Popup de editar cita
-    const closePopupEditarCita = document.getElementById('closePopupEditarCita');  // Botón "x" de cerrar en el popup editar cita
-    const popupConfirmarCambios = document.getElementById('popupConfirmarCambios');
-    const cancelarConfirmacionBtn = document.getElementById('cancelarConfirmacionBtn');
-    const confirmarCambiosBtn = document.getElementById('confirmarCambiosBtn');
-    const confirmarBtn = document.getElementById('confirmarBtn'); // Botón que abre este popup
-    // Selecciona el botón de eliminar cita
-    const eliminarBtn = document.getElementById('eliminarBtn');
     let currentDate = new Date();
     let currentView = 'month';  // 'month' o 'week'
     let citas = []; // Variable global para almacenar las citas
@@ -88,60 +78,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         calendarHTML += '</div>';
-        calendar.innerHTML = calendarHTML;
+        calendar.innerHTML = calendarHTML;}
 
-        // // Añadir el evento de clic a cada día
-        // btnAgendar.addEventListener('click', function() {
-        //     popup.style.display = "block";
-        // });
-        document.querySelectorAll(".day").forEach(day => {
-            day.addEventListener("click", function () {
-                const selectedDate = this.getAttribute("data-fecha");
-                document.getElementById("fechaSeleccionada").textContent = selectedDate;
-                document.getElementById("fecha").value = selectedDate; // Asigna la fecha al campo de fecha del formulario
-                // Obtener las citas del día seleccionado
-                const citasDelDia = obtenerCitasPorFecha(selectedDate);
-                // if (citasDelDia.length > 0) {
-                //     mostrarPopupCitas(citasDelDia);  // Muestra el popup si hay citas
-                // } else {
-                //     popup.style.display = "block"; // Muestra el popup
-                // }
-            });
-        });
-        // // Evento para editar cita
-        // document.getElementById("popup-content").addEventListener("click", function (event) {
-        //     if (event.target.classList.contains("cita-item")) {  
-        //         const citaId = event.target.getAttribute("data-cita-id");
-        //         const cita = citas.find(cita => cita.id === parseInt(citaId));
-        //         if (cita) {
-        //             // Mostrar el popup para editar la cita
-        //             document.getElementById("popupEditarCita").classList.add("mostrar");
-        //             console.log("Popup para editar la cita mostrado");
-        //             // Rellenar los campos del formulario con los datos de la cita
-        //             document.getElementById("cita_id").value = cita.id;
-        //             document.getElementById("titulo").value = cita.titulo;
-        //             document.getElementById("paciente").value = cita.paciente.id;
-        //             document.getElementById("fecha").value = cita.fecha;
-        //             document.getElementById("hora").value = cita.hora;
-        //             document.getElementById("sala").value = cita.sala;
-        //             document.getElementById("detalle").value = cita.descripcion;
-        //         }
-        //     }
-        // });
-        
-        // // Cerrar popup de edición con la "x"
-        // closePopupEditarCita.addEventListener("click", function () {
-        //     popupEditarCita.classList.remove("mostrar");  // Ocultar el popup
-        // });
-    }
-
-    // // Cerrar el popup de edición al hacer clic fuera del mismo
-    // window.addEventListener("click", function (event) {
-    //     if (event.target === popupEditarCita) {
-    //         popupEditarCita.classList.remove("mostrar");
-    //     }
-    // });
-    
     //------------------------------------VISTA SEMANAL------------------------------------
     function generateWeekView() {
         const startOfWeek = getStartOfWeek(currentDate);
@@ -205,20 +143,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return monthNames[monthIndex];
     }
 
-    //------------------------------------FUNCION DESTACAR CITAS------------------------------------
-    function destacarDiasConCita(fechasCitas) {
-        const diasDelCalendario = document.querySelectorAll('.day');
-        diasDelCalendario.forEach(dia => {
-            const fechaDia = dia.getAttribute('data-fecha');  // La fecha del día en formato "dd/mm/yyyy"
-            // Convertir la fecha del calendario al formato "YYYY-MM-DD" para comparar con las fechas de las citas
-            const [diaNum, mesNum, anio] = fechaDia.split('/');
-            const fechaFormateada = `${anio}-${mesNum.padStart(2, '0')}-${diaNum.padStart(2, '0')}`;
-            // Comparar la fecha formateada con las fechas de las citas
-            if (fechasCitas.includes(fechaFormateada)) {
-                dia.classList.add('dia-con-cita');  // Destacar el día con una clase CSS especial
-            }
-        });
-    }
+
     //------------------------------------EVENTOS DE LOS BOTONES------------------------------------
     prevButton.addEventListener('click', function () {
         if (currentView === 'month') {
@@ -254,142 +179,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     
-    //------------------------------------CERRAR POPUP CITAS------------------------------------
-    window.addEventListener("click", function (event) {
-        if (event.target === popup) {
-            popup.style.display = "none";
-        }
-    });
-
     updateCalendar();
-    // // Función para mostrar el popup con las citas
-    // function mostrarPopupCitas(citasDelDia) {
-    //     const popupCitas = document.getElementById("popupCitas");
-    //     const popupCitasContent = document.getElementById("popup-content");
-    //     // Limpiar el contenido previo
-    //     popupCitasContent.innerHTML = '';
-    //     // Agregar las citas al popup
-    //     citasDelDia.forEach(cita => {
-    //         const citaElement = document.createElement('div');
-    //         citaElement.classList.add('cita-item');
-    //         citaElement.setAttribute('data-cita-id', cita.id);  // Agregar el dataset con el ID de la cita
-    //         citaElement.innerHTML = `
-    //             <p> ${cita.titulo}</p>
-    //             <p> ${cita.hora}</p>
-    //             <p> Descripción: ${cita.descripcion}</p>
-    //         `;
-    //         popupCitasContent.appendChild(citaElement);
 
-    //         // Agregar el evento de clic a todo el contenedor #popup-content
-    //         document.getElementById("popup-content").addEventListener("click", function (event) {
-    //             if (event.target.classList.contains("cita-item") || event.target.parentNode.classList.contains("cita-item")) {
-    //                 const citaId = event.target.getAttribute("data-cita-id") || event.target.parentNode.getAttribute("data-cita-id");
-    //                 const cita = citas.find(c => c.id === parseInt(citaId));
-    //                 if (cita) {
-    //                     // Mostrar el popup para editar la cita
-    //                     document.getElementById("popupEditarCita").classList.add("mostrar");
-    //                     console.log("Popup para editar la cita mostrado");
-            
-    //                     // Rellenar los campos del formulario con los datos de la cita
-    //                     document.getElementById("cita_id").value = cita.id;
-    //                     document.getElementById("titulo").value = cita.titulo;
-    //                     document.getElementById("paciente").value = cita.paciente.id;
-    //                     document.getElementById("fecha").value = cita.fecha;
-    //                     document.getElementById("hora").value = cita.hora;
-    //                     document.getElementById("sala").value = cita.sala;
-    //                     document.getElementById("detalle").value = cita.descripcion;
-    //                 }
-    //             }
-    //         });
-    //     });
-
-    //     // Mostrar el popup con las citas
-    //     popupCitas.style.display = 'block';
-    // }
-    // // Función para obtener las citas del día seleccionado
-    // function obtenerCitasPorFecha(fechaDia) {
-    //     const [dia, mes, anio] = fechaDia.split('/');
-    //     const fechaFormateada = `${anio}-${mes.padStart(2, '0')}-${dia.padStart(2, '0')}`;
-
-    //     // Filtrar citas que coinciden con esta fecha
-    //     return citas.filter(cita => cita.fecha === fechaFormateada);
-    // }
-    // Evento para redireccionar a semana
-    document.querySelectorAll(".day").forEach(day => {
-        day.addEventListener("click", function () {
-            const selectedDate = this.getAttribute("data-fecha");
-            const [dia, mes, anio] = selectedDate.split('/');
-            const fechaFormateada = `${anio}-${mes.padStart(2, '0')}-${dia.padStart(2, '0')}`;
-            const fecha = new Date(fechaFormateada);
-            currentDate = fecha;
-            currentView = 'week';
-            updateCalendar();
-        });
-    });
-
-    // // Función para abrir el popup de confirmación
-    // function abrirPopupConfirmacion() {
-    //     popupConfirmarCambios.style.display = 'flex';
-    // }
-
-    // // Escucha para cualquier clic dentro del documento
-    // document.addEventListener('click', function (event) {
-    //     if (event.target && event.target.id === 'confirmarBtn') {
-    //         event.preventDefault();
-    //         console.log('Botón Confirmar clickeado');
-    //         abrirPopupConfirmacion();  // Aquí abrirás el popup
-    //     }
-    // });
-
-    // // Escucha para el clic en el botón Confirmar del formulario de edición de citas
-    // confirmarBtn.addEventListener('click', function (event) {
-    //     event.preventDefault(); // Evita el envío del formulario inmediatamente
-    //     abrirPopupConfirmacion(); // Abre el popup cuando se presiona el botón Confirmar
-    // });
-
-    // // Cerrar el popup de confirmación al presionar "Cancelar"
-    // cancelarConfirmacionBtn.addEventListener('click', function () {
-    //     popupConfirmarCambios.style.display = 'none';  // Cierra el popup
-    // });
-
-    // // Confirmar cambios y cerrar el popup
-    // confirmarCambiosBtn.addEventListener('click', function () {
-    //     // Cierra el popup y envía el formulario
-    //     popupConfirmarCambios.style.display = 'none';  
-    //     document.getElementById('formEditarCita').submit();  // Enviar el formulario después de la confirmación
-    // });
-
-    // // Cerrar el popup si se hace clic fuera del contenido
-    // window.addEventListener('click', function (event) {
-    //     if (event.target === popupConfirmarCambios) {
-    //         popupConfirmarCambios.style.display = 'none';  // Cerrar si se hace clic fuera del popup
-    //     }
-    // });
-
-    // // Agrega el evento de clic para el botón de eliminar cita
-    // eliminarBtn.addEventListener('click', function () {
-    //     const citaId = document.getElementById('cita_id').value; // Obtener el ID de la cita
-
-    //     if (confirm('¿Estás seguro de que deseas eliminar esta cita?')) {
-    //         // Realizar una solicitud AJAX para eliminar la cita
-    //         fetch(`/eliminar-cita/${citaId}/`, {
-    //             method: 'DELETE',
-    //             headers: {
-    //                 'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
-    //                 'Content-Type': 'application/json'
-    //             }
-    //         })
-    //         .then(response => {
-    //             if (response.ok) {
-    //                 alert('Cita eliminada exitosamente.');
-    //                 location.reload(); // Recargar la página para actualizar el calendario
-    //             } else {
-    //                 alert('Hubo un error al eliminar la cita.');
-    //             }
-    //         })
-    //         .catch(error => {
-    //             console.error('Error al eliminar la cita:', error);
-    //         });
-    //     }
-    // });
 });
