5993809d5e502718e902fcc643dfa12f677ae961
crispardo2 <cristobal.pardo@cloud.uautonoma.cl>
2024-11-14 00:08:00 -0400

    implementacion en admin django para agregar múltiples roles al usuario

--- administrador/admin.py
+++ administrador/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Administrador
 
-# Register your models here.
+@admin.register(Administrador)
+class AdministradorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fecha_contratacion', 'departamento')
+    search_fields = ('user__first_name', 'user__last_name', 'departamento')
+    list_filter = ('fecha_contratacion', 'departamento')
--- terapeuta/admin.py
+++ terapeuta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Terapeuta
 
-# Register your models here.
+@admin.register(Terapeuta)
+class TerapeutaAdmin(admin.ModelAdmin):
+    list_display = ('user', 'especialidad', 'fecha_ingreso', 'disponibilidad', 'horas_trabajadas')
+    search_fields = ('user__first_name', 'user__last_name', 'especialidad')
+    list_filter = ('disponibilidad', 'fecha_contratacion')
